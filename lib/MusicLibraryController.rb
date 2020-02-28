1b910fd326d29a1d39ada85306e61deeafd1c588
fadimata tillman <fadi.tillman@gmail.com>
2020-02-28 21:05:51 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -15,15 +15,15 @@ class MusicLibraryController
     input = gets.strip
     if  input != "exit" 
 
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
+      puts "Welcome to your music library!"*4
+      puts "To list all of your songs, enter 'list songs'."*4
+      puts "To list all of the artists in your library, enter 'list artists'."*4
+      puts "To list all of the genres in your library, enter 'list genres'."*4
+      puts "To list all of the songs by a particular artist, enter 'list artist'."*4
+      puts "To list all of the songs of a particular genre, enter 'list genre'."*4
+      puts "To play a song, enter 'play song'."*4
+      puts "To quit, type 'exit'."*4
+      puts "What would you like to do?"*4
 
 
 
