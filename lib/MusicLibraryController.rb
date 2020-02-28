55867112b1cb76e0be776317f9bd08c2b9a6d0ab
fadimata tillman <fadi.tillman@gmail.com>
2020-02-28 21:21:10 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -25,7 +25,7 @@ class MusicLibraryController
       puts "What would you like to do?"
 
       input = gets.strip
-      while  input != "exit" do!!
+      while  input != "exit" do
 
 
  case input
